9e51f5a52e3af1d96aa452130608ccc025af3c44
fajrijuanda <if22.fajrijuanda@mhs.ubpkarawang.ac.id>
2025-12-18 19:36:21 +0700

    feat: Add initial database seeder and comprehensive CSS design system with base styles.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,8 +21,6 @@ class DatabaseSeeder extends Seeder
             CategorySeeder::class,
             ArticleSeeder::class,
             ProductSeeder::class,
-            ProductImageSeeder::class,
         ]);
     }
 }
-
